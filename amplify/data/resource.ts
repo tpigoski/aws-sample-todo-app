ced75344ab6304ba166111dcf4bfb35bfb67ae41
Tom Pigoski <tpigoski@gmail.com>
2025-05-28 15:13:37 -0700

    added per user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,9 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
+  Todo: a.model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+    }).authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,11 +17,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -26,8 +26,8 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <button onClick={createTodo}>Add To Do</button>
       <ul>
         {todos.map((todo) => (
           <li 
